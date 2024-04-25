759b320586251ceca616596f8535711fb009f0c2
Tri <tri@MacBook-Air-cua-Tri.local>
2024-04-25 22:32:37 +0700

    update

--- front-end/src/views/nav/NavBar.vue
+++ front-end/src/views/nav/NavBar.vue
@@ -4,13 +4,15 @@
       ><span class="nav-item">Trang Người dùng</span></router-link
     >
     <router-link to="/product"><span class="nav-item">Sách</span> </router-link>
-    <router-link to="/checkBorrow"
-      ><span class="nav-item">Sinh viên mượn sách</span>
-    </router-link>
     <router-link to="/publishing"
       ><span class="nav-item">Nhà xuất bản</span>
     </router-link>
-    <span @click="logout" class="nav-item logout">Logout</span>
+    <router-link to="/checkBorrow"
+      ><span class="nav-item">Sinh viên mượn sách</span>
+    </router-link>
+    <span @click="logout" class="nav-item logout"
+      >Đăng xuất <i class="fa-solid fa-right-from-bracket"></i
+    ></span>
   </nav>
   <router-view />
 </template>
